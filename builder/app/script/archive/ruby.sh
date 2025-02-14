388ea868b4825ccdc66aeaf91b73ba98e9025fb4
taehun-kmu <taehun@kookmin.ac.kr>
2025-02-14 17:02:56 +0900

    add ROS

--- builder/app/script/archive/ruby.sh
+++ builder/app/script/archive/ruby.sh
@@ -4,6 +4,8 @@ set -e
 git clone -b v0.9.4 --recursive --depth=1 https://github.com/postmodern/ruby-install.git $HOME/.ruby-install
 cd $HOME/.ruby-install && make install
 
+export MAKEFLAGS="-j$(nproc)"
+
 ruby-install --update
 ruby-install --install-dir $HOME/.ruby ruby 3.3.6
 
