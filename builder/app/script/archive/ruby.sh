8bbc73446611efe32d5be23ea7c0b89ef6dbf69e
taehun-kmu <taehun@kookmin.ac.kr>
2025-02-19 13:51:36 +0900

    fix builder/app/script/archive/ruby.sh

--- builder/app/script/archive/ruby.sh
+++ builder/app/script/archive/ruby.sh
@@ -1,10 +1,53 @@
 #!/bin/bash
 set -e
 
-git clone--recursive --depth=1 https://github.com/rbenv/ruby-build.git $HOME/.ruby-build
+packages=(
+  git
+  autoconf
+  patch
+  build-essential
+  libssl-dev
+  libyaml-dev
+  libreadline6-dev
+  zlib1g-dev
+  libgmp-dev
+  libncurses5-dev
+  libffi-dev
+  libgdbm6
+  libgdbm-dev
+  libdb-dev
+  uuid-dev
+)
+
+if [[ $(id -u) -eq 0 ]]; then
+  sudo_cmd=""
+else
+  sudo_cmd="sudo"
+fi
+
+if command -v nala &> /dev/null; then
+  packages_manager="nala"
+else
+  packages_manager="apt"
+fi
+
+for pkg in "${packages[@]}"; do
+  if ! dpkg -s "$pkg" &> /dev/null; then
+    $sudo_cmd $packages_manager update
+    $sudo_cmd $packages_manager install -y "$pkg"
+  else
+    echo "Already $pkg package"
+  fi
+done
+
+git clone --recursive --depth=1 https://github.com/rbenv/ruby-build.git $HOME/.ruby-build
 cd $HOME/.ruby-build
 
-PREFIX=/usr/local $HOME/.ruby-build/install.sh
+if ! PREFIX=/usr/local "$HOME/.ruby-build/install.sh" &> /dev/null; then
+ $sudo_cmd PREFIX=/usr/local $HOME/.ruby-build/install.sh
+else
+  echo "Installation Complete"
+fi
 
 ruby-build --list
 ruby-build 3.3.7 $HOME/.ruby
