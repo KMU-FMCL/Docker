059c3c8e9d181c1796b65eb7ec524da093a0db78
taehun-kmu <taehun@kookmin.ac.kr>
2025-02-19 13:25:15 +0900

    fix builder/app/script/archive/node.sh

--- builder/app/script/archive/node.sh
+++ builder/app/script/archive/node.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 set -e
 
+if [[ $(id -u) -eq 0 ]]; then
+  sudo_cmd=""
+else
+  sudo_cmd="sudo"
+fi
+
 git clone -b v5.3.22 --recursive --depth=1 https://github.com/nodenv/node-build.git $HOME/.node-build
 
-PREFIX=/usr/local $HOME/.node-build/install.sh
+if ! PREFIX=/usr/local "$HOME/.node-build/install.sh" &> /dev/null; then
+  $sudo_cmd PREFIX=/usr/local "$HOME/.node-build/install.sh"
+else
+  echo "Installation Complete"
+fi
+
 node-build 22.12.0 $HOME/.node
 
 export PATH=$HOME/.node/bin:$PATH
